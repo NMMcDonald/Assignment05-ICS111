14ef63b5683e4d917458f2f347978c3fd004ef30
Nicolas McDonald <nicolasmcdonald@Nicolass-MacBook-Air-3.local>
2022-09-27 20:53:35 -1000

    started scanning for user input

--- Assignment05.java
+++ Assignment05.java
@@ -7,15 +7,37 @@ import java.util.Scanner;
 
 public class Assignment05 {
     public static void main(String args[]) {
-        
+        Scanner scan = new Scanner(System.in);​
+        String name="";
+
+        System.out.println("Voter name: "+name);
+
+
     }
 }
 
 class Voter {
-    private static int nVoters=0;
+    private static int nVoters;
     private String name="";
-    private name=n;
     private String voterID="";
     
-    Voter v = new Voter(n);
+    Voter v = new Voter("");
+
+    Voter(String n) {
+        name=n;
+        nVoters++;
+        voterID="HI"+voterID.length();
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public static int getnVoters() {
+        return nVoters;
+    }
+
+    public String toString() {
+        return voterID+name;
+    }
 }
