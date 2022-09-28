91a32aaf11154f6aa36c64f46d0e2ea621c46652
NMMcDonald <97976426+NMMcDonald@users.noreply.github.com>
2022-09-27 16:54:51 -1000

    Create Assignment05.java

---
+++ Assignment05.java
@@ -0,0 +1,21 @@
+/*
+Assignment05
+Nicolas McDonald
+ICS111 004
+*/
+import java.util.Scanner;
+
+public class Assignment05 {
+    public static void main(String args[]) {
+        
+    }
+}
+
+class Voter {
+    private static int nVoters=0;
+    private String name="";
+    private name=n;
+    private String voterID="";
+    
+    Voter v = new Voter(n);
+}
