713860a21be7b52c3dd7d3c0e77b256d0a79f478
Nicolas McDonald <nicolasmcdonald@dhcp-168-105-253-138.wireless.manoa.hawaii.edu>
2022-09-28 10:21:35 -1000

    set up the voter constructor more and added the three contrusctors i'll be using

--- Assignment05.java
+++ Assignment05.java
@@ -7,26 +7,28 @@ import java.util.Scanner;
 
 public class Assignment05 {
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);​
-        String name="";
-
-        System.out.println("Voter name: "+name);
-
-
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        Voter v1 = new Voter(name);
+        name = scan.nextLine();
+        Voter v2 = new Voter(name);
+        name = scan.nextLine();
+        Voter v3 = new Voter(name);
+        scan.close();
     }
 }
 
 class Voter {
     private static int nVoters;
+    private int voterNumber;
     private String name="";
     private String voterID="";
     
-    Voter v = new Voter("");
-
     Voter(String n) {
         name=n;
         nVoters++;
-        voterID="HI"+voterID.length();
+        voterNumber=nVoters;
+        voterID="HI"+n.length();
     }
 
     /**
@@ -38,6 +40,6 @@ class Voter {
     }
 
     public String toString() {
-        return voterID+name;
+        return voterID + " "+ name;
     }
 }
