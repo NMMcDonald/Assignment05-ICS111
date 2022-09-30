ef01cbe13f0c4bc19ced32edad2f3e6577a8c472
Nicolas McDonald <nicolasmcdonald@dhcp-168-105-229-161.wireless.manoa.hawaii.edu>
2022-09-30 10:20:39 -1000

    Final version. When the voter ID is made it isnt showing the correct numbering system it should be showing the 8bit system but it isnt.

--- Assignment05.java
+++ Assignment05.java
@@ -8,29 +8,22 @@ import java.util.Scanner;
 public class Assignment05 {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
-        String name ="";
-        
-
-        Voter v1 = new Voter(name);
-        Voter v2 = new Voter(name);
-        Voter v3 = new Voter(name);
-
+       
+        System.out.print("Voter name: ");
         String next = scan.nextLine();
-        System.out.println("Voter name: " + next);
-        System.out.println("Voter " + Voter.getnVoters + " created.");
-        System.out.println("Voter name: " + next);
-        System.out.println("Voter " + Voter.getnVoters + " created.");
-        System.out.println("Voter name: ");
-        System.out.println("Voter " + Voter.getnVoters + " created.");
-        /*
-        System.out.println("Voter name: ");
-        name = scan.nextLine();
-        System.out.println("Voter " + Voter.getnVoters + " created.");
-
-        System.out.println("Voter name: ");
-        name = scan.nextLine();
-        System.out.println("Voter " + Voter.getnVoters + " created.");
-        */
+        Voter v1 = new Voter(next);
+        System.out.println("Voter " + Voter.getnVoters() + " created.");
+       
+        System.out.print("Voter name: ");
+        next = scan.nextLine();
+        Voter v2 = new Voter(next);
+        System.out.println("Voter " + Voter.getnVoters() + " created.");
+       
+        System.out.print("Voter name: ");
+        next = scan.nextLine();
+        Voter v3 = new Voter(next);
+        System.out.println("Voter " + Voter.getnVoters() + " created.");
+        
         System.out.println(v1);
         System.out.println(v2);
         System.out.println(v3);
@@ -41,25 +34,32 @@ public class Assignment05 {
 
 class Voter {
     public static String getnVoters;
-    private static int nVoters;
+    private static int nVoters =0;
     private String name="";
     private String voterID="";
     
+    /**
+     * Accepts the name, ID, and number of the voter.
+     * @param n stores voter information.
+     */
     Voter(String n) {
         name=n;
         nVoters++;
         voterID="HI"+n.length();
     }
 
-    /**
-     * 
-     * @return
+    /*
+     * The number of voters in a string data type.
+     * @return the number of voters.
      */
     public static int getnVoters() {
         return nVoters;
     }
 
-    public String toString() { //use toString in your main method when you call it at the end
+    /* Returns the whole ID of each voter.
+     * @return voters ID and the name they input.
+     */
+    public String toString() { 
         return voterID + " "+ name;
     }
 }
